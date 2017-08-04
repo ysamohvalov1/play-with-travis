580b2c18cb8ee0c91eae66cb223d2d2f98195e0e
Artem Starostenko <artemstarostenko65@gmail.com>
2017-08-05 02:46:46 +0300

    Break tests again

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import unittest
 class TestDevOps(unittest.TestCase):
 
     def test_conflict(self):
-        self.assertEqual('Dev' + 'Ops', 'DevOps')
+        self.assertEqual('Dev' + 'Ops', 'Conflict')
 
 if __name__ == '__main__':
     unittest.main()
